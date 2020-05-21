b71815e203c1baeb14447894a751c16dabff179d
rikiahmad2 <ahmadriki9512@gmail.com>
2020-05-21 11:38:08 +0700

    Versi 1
    Software Akuntansi Desktop

---
+++ src/akuntansi/Pengguna.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akuntansi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Chutenks
+ */
+public class Pengguna {
+
+    public Connection con;
+    public PreparedStatement pst;
+    
+    //query login ke database
+    public void login() throws SQLException, ClassNotFoundException{
+        Class.forName("com.mysql.jdbc.Driver");  
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/akuntansi","root","");
+            String sql = "Select * from user where username=? and password =?";
+            pst = con.prepareStatement(sql);
+    }
+    
+}
